80ca3e66c75307579d3e786eec03d8217e232309
stas.granin <stas.granin@touricoholidays.com>
2019-05-23 09:51:46 +0300

    First commit - directory structure and necessary files

---
+++ shared-ui-components/projects/shared-ui-components-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of shared-ui-components-lib
+ */
+
+export * from './lib/shared-ui-components-lib.service';
+export * from './lib/shared-ui-components-lib.component';
+export * from './lib/shared-ui-components-lib.module';
