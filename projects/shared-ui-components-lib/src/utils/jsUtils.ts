0edf05c4be475a3eae2c139311a2fa6df4b24280
Kosta Masliansky <kosta.masliansky@hotelbeds.com>
2019-12-24 13:55:52 +0200

    fix deep comparison

--- projects/shared-ui-components-lib/src/utils/jsUtils.ts
+++ projects/shared-ui-components-lib/src/utils/jsUtils.ts
@@ -1,4 +1,3 @@
-
 // @dynamic
 export class JsUtils {
   static deepCopy = (obj) => {
@@ -45,51 +44,7 @@ export class JsUtils {
   }
 
   static deepObjectComparision = (o, p) => {
-    let i;
-    const keysO = Object.keys(o).sort();
-    const keysP = Object.keys(p).sort();
-
-    if (keysO.length !== keysP.length) {
-      return false; // not the same nr of keys
-    }
-    if (keysO.join('') !== keysP.join('')) {
-      return false; // different keys
-    }
-    for (i = 0; i < keysO.length; ++i) {
-      if (o[keysO[i]] instanceof Array) {
-        if (!(p[keysO[i]] instanceof Array)) {
-          return false;
-        }
-        if (p[keysO[i]].sort().join('') !== o[keysO[i]].sort().join('')) {
-          return false;
-        }
-      } else if (o[keysO[i]] instanceof Date) {
-        if (!(p[keysO[i]] instanceof Date)) {
-          return false;
-        }
-        if (('' + o[keysO[i]]) !== ('' + p[keysO[i]])) {
-          return false;
-        }
-      } else if (o[keysO[i]] instanceof Function) {
-        if (!(p[keysO[i]] instanceof Function)) {
-          return false;
-        }
-      } else if (o[keysO[i]] instanceof Object) {
-        if (!(p[keysO[i]] instanceof Object)) {
-          return false;
-        }
-        if (o[keysO[i]] === o) { // self reference?
-          if (p[keysO[i]] !== p) {
-            return false;
-          }
-        } else if (JsUtils.deepObjectComparision(o[keysO[i]], p[keysO[i]]) === false) {
-          return false; // WARNING: does not deal with circular refs other than ^^
-        }
-      } else if (o[keysO[i]] !== p[keysO[i]]) { // change !== to != for loose comparison
-        return false; // not the same value
-      }
-    }
-    return true;
+    return JSON.stringify(o) === JSON.stringify(p);
   }
 
   static isEmpty = (arrayOrObject) => {
@@ -107,7 +62,7 @@ export class JsUtils {
   static capitalize = (str): string => str.charAt(0).toUpperCase() + str.slice(1)
 
   static arrayToObject = (array: any[], id: any, withCollisions?: boolean) => {
-     return array.reduce((obj, item) => {
+    return array.reduce((obj, item) => {
       if (withCollisions) {
         if (!obj[item[id]]) {
           obj[item[id]] = [item];
