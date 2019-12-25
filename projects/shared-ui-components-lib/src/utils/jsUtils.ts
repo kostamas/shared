ad194b6f0f88650cd3071ac1199d77100bdb4a33
Kosta Masliansky <kosta.masliansky@hotelbeds.com>
2019-12-25 16:35:44 +0200

    fix deep comparison fn

--- projects/shared-ui-components-lib/src/utils/jsUtils.ts
+++ projects/shared-ui-components-lib/src/utils/jsUtils.ts
@@ -44,55 +44,47 @@ export class JsUtils {
   }
 
   static deepObjectComparision = (o, p) => {
-    let i;
-    const keysO = Object.keys(o).sort();
-    const keysP = Object.keys(p).sort();
-
-    if (keysO.length !== keysP.length) {
-      return false; // not the same nr of keys
-    }
-    if (keysO.join('') !== keysP.join('')) {
-      return false; // different keys
-    }
-    for (i = 0; i < keysO.length; ++i) {
-      if (o[keysO[i]] instanceof Array) {
-        if (!(p[keysO[i]] instanceof Array)) {
-          return false;
-        }
-        const len = o[keysO[i]].length;
-        if (len !== p[keysO[i]].length) {
-          return false
-        }
-        for (let a = 0; a < len; a++) {
-          if (JsUtils.deepObjectComparision(o[keysO[a]], p[keysO[a]]) === false) {
-            return false
-          }
-        }
-      } else if (o[keysO[i]] instanceof Date) {
-        if (!(p[keysO[i]] instanceof Date)) {
-          return false;
-        }
-        if (('' + o[keysO[i]]) !== ('' + p[keysO[i]])) {
-          return false;
-        }
-      } else if (o[keysO[i]] instanceof Function) {
-        if (!(p[keysO[i]] instanceof Function)) {
-          return false;
-        }
-      } else if (o[keysO[i]] instanceof Object) {
-        if (!(p[keysO[i]] instanceof Object)) {
-          return false;
-        }
-        if (o[keysO[i]] === o) { // self reference?
-          if (p[keysO[i]] !== p) {
-            return false;
-          }
-        } else if (JsUtils.deepObjectComparision(o[keysO[i]], p[keysO[i]]) === false) {
-          return false; // WARNING: does not deal with circular refs other than ^^
-        }
-      } else if (o[keysO[i]] !== p[keysO[i]]) { // change !== to != for loose comparison
-        return false; // not the same value
+    if (o instanceof Object && p instanceof Object && !Array.isArray(o) && !Array.isArray(p)) {
+      let i;
+      const keysO = Object.keys(o).sort();
+      const keysP = Object.keys(p).sort();
+      if (keysO.length !== keysP.length) {
+        return false; // not the same nr of keys
       }
+      if (keysO.join('') !== keysP.join('')) {
+        return false; // different keys
+      }
+      for (i = 0; i < keysO.length; ++i) {
+        if (JsUtils.deepObjectComparision(o[keysO[i]], p[keysO[i]]) === false) {
+          return false;
+        }
+      }
+    } else if (o instanceof Array) {
+      if (!(p instanceof Array)) {
+        return false;
+      }
+      const len = o.length;
+      if (len !== p.length) {
+        return false;
+      }
+      for (let a = 0; a < len; a++) {
+        if (JsUtils.deepObjectComparision(o[a], p[a]) === false) {
+          return false;
+        }
+      }
+    } else if (o instanceof Date) {
+      if (!(p instanceof Date)) {
+        return false;
+      }
+      if (('' + o) !== ('' + p)) {
+        return false;
+      }
+    } else if (o instanceof Function) {
+      if (!(p instanceof Function)) {
+        return false;
+      }
+    } else {
+      return o === p;
     }
     return true;
   }
